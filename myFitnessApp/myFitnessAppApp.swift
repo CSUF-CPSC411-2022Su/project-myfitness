28a06958309ce4bfdf026c0fa5dea650bd02a00c
Luis Alvarado <luisalvarado@csu.fullerton.edu>
2022-06-08 14:07:38 -0700

    Initial Commit

---
+++ myFitnessApp/myFitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFitnessAppApp.swift
+//  myFitnessApp
+//
+//  Created by Luis Alvarado on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct myFitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
