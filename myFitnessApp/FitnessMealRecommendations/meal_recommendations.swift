e7365aaa50f03cd3014e8e5af3c8958dc248fb37
Luis Alvarado <luisalvarado@csu.fullerton.edu>
2022-06-08 14:30:03 -0700

    added MealRecommendationFeature

---
+++ myFitnessApp/FitnessMealRecommendations/meal_recommendations.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  myFitnessApp
+//
+//  Created by Luis Alvarado on 6/8/22.
+//
+
+import Foundation
+
+struct MealRecommendation {
+    //Things that need to be added to Feature 3:
+    //1. look for an API that has recipes that are healthy.
+    //2. Create a view controller and start to design how you want viewcontroller to look like.
+    //3. Implement functions that will help with feature and write comments as you go to make sure you understand what is happening in the code.
+    //4.Unit testing once functions are created to ensure feature is working correctly.
+}
