dbfd23e4f7d64d37b77566e977341bb79f73cb82
Dacgameboy <dacortez101@csu.fullerton.edu>
2022-06-11 18:37:16 -0700

    added borders

--- myFitnessApp/ContentView.swift
+++ myFitnessApp/ContentView.swift
@@ -19,17 +19,26 @@ struct ContentView: View {
         VStack {
             TextField("Weight", text: $weight)
                 .padding()
+                .border(.gray)
                 .keyboardType(.decimalPad)
             TextField("Height", text: $height)
                 .padding()
+                .border(.gray)
                 .keyboardType(.decimalPad)
             TextField("Age", text: $age)
                 .padding()
+                .border(.gray)
                 .keyboardType(.decimalPad)
             Picker("Exercise", selection: $exercise) {
                 Text("Little To None").tag(Exercise.littleToNone)
                 Text("Light").tag(Exercise.light)
+                Text("Moderate").tag(Exercise.moderate)
+                Text("Active").tag(Exercise.active)
+                Text("Very Active").tag(Exercise.veryActive)
             }
+            .padding()
+            .border(.gray, width: 3)
+            .pickerStyle(.menu)
         }
     }
 }
