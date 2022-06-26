1dae1d2f923e86c00633ee5996825e373d7713c1
Erica <ericab90@csu.fullerton.edu>
2022-06-26 12:32:14 -0700

    add file

--- myFitnessApp/Strength.swift
+++ myFitnessApp/Strength.swift
@@ -9,22 +9,47 @@ import Foundation
 import SwiftUI
 struct StrengthView: View {
     var body: some View {
-        //NavigationView {
-            List {
+        NavigationView {
+            VStack {
                 (Text("Choose strength workout"))
-                    .padding()
                 NavigationLink(destination: CoreView()) {
-                    Text("Core")
+                    Image("abs")
+                        .resizable()
+                        .scaledToFit()
                 }
-                NavigationLink(destination: ArmsView()) {
-                    Text("Arms")
+                ScrollView {
+                    HStack {
+                        NavigationLink(destination: ArmsView()) {
+                            Image("abs")
+                                .resizable()
+                                .scaledToFit()
+                            Image("abs")
+                                .resizable()
+                                .scaledToFit()
+                            Image("abs")
+                                .resizable()
+                                .scaledToFit()
+                            Image("abs")
+                                .resizable()
+                                .scaledToFit()
+                            
+                        
+                    }
+              
                 }
+                }
+                ScrollView {
+                    HStack {
                 NavigationLink(destination: LegsView()) {
                     Text("Legs")
                 }
-        }
-    }
-}
+                }
+                }
+                
+            }
+        }//End of Navigation view
+    }//End of body View
+}//End of View struct
 struct Strength_Previews: PreviewProvider {
     static var previews: some View {
         StrengthView()
