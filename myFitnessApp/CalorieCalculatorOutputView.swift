2d863f7ceff0802365e1926b0177e5e0b2254025
Dacgameboy <dacortez101@csu.fullerton.edu>
2022-06-26 11:56:27 -0700

    MUCH BETTER FORMAT

--- myFitnessApp/CalorieCalculatorOutputView.swift
+++ myFitnessApp/CalorieCalculatorOutputView.swift
@@ -11,23 +11,26 @@ import SwiftUI
 struct CalorieCalculatorOutputView: View {
     @ObservedObject var userInfo: UserInfo
     var body : some View {
-        HStack (alignment: .top) {
-            VStack {
-                Text("Reccomended Calorie Intake: ")
-                    .padding()
-                HStack {
-                    Text("Maintain Weight:")
-                    Text("\(userInfo.BMR) calories/day")
+            List {
+                
+                Section ("Maintain Weight") {
+                        Text("\(userInfo.BMR) calories/day")
                 }
-                .padding()
-                HStack {
-                    Text("Lose Weight (~1 pound / week):")
-                    let weightlossBMR = userInfo.BMR - 500
-                    Text("\(weightlossBMR) calories/day")
+                Section ("Lose Weight (~1 pound / week)"){
+                        let weightlossBMR = userInfo.BMR - 500
+                        Text("\(weightlossBMR) calories/day")
                 }
             }
             
-        }
        
+    }
+}
+
+
+struct fancy: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .border(Color.black)
     }
 }
--- myFitnessApp/ContentView.swift
+++ myFitnessApp/ContentView.swift
@@ -23,56 +23,40 @@ struct CalorieCalculatorInput: View {
     @State var gender: Gender = .male
     var body: some View {
         NavigationView {
-            VStack
-            {
-                HStack {
-                    Text("Weight:")
-                    TextField("Weight in TBD", text: $weight)
-                        .padding()
-                        .border(.gray)
-                        .keyboardType(.decimalPad)
+            VStack {
+                Form
+                {
+                    Section ("Weight") {
+                        TextField("Weight in TBD", text: $weight)
+                            .keyboardType(.decimalPad)
+                    }
+                    Section ("Height") {
+                        TextField("Height in TBD", text: $height)
+                            .keyboardType(.decimalPad)
+                    }
+                    Section ("Age") {
+                        TextField("Age in Years", text: $age)
+                            .keyboardType(.decimalPad)
+                    }
+                    Section ("Exercise") {
+                        Picker("Exercise", selection: $exercise) {
+                            Text("Little To None").tag(Exercise.littleToNone)
+                            Text("Light").tag(Exercise.light)
+                            Text("Moderate").tag(Exercise.moderate)
+                            Text("Active").tag(Exercise.active)
+                            Text("Very Active").tag(Exercise.veryActive)
+                        }
+                    }
+                    Section ("Gender") {
+                        Picker("Gender", selection: $gender) {
+                            Text("Male").tag(Gender.male)
+                            Text("Female").tag(Gender.female)
+                        }.pickerStyle(.segmented)
+                    }
                 }
-                HStack {
-                    Text("Height:")
-                    TextField("Height in TBD", text: $height)
-                        .padding()
-                        .border(.gray)
-                        .keyboardType(.decimalPad)
-                }
-                HStack {
-                    Text("Age:")
-                    TextField("Age in Years", text: $age)
-                        .padding()
-                        .border(.gray)
-                        .keyboardType(.decimalPad)
-                }
-                HStack {
-                    Text("Activity:")
-                    Picker("Exercise", selection: $exercise) {
-                        Text("Little To None").tag(Exercise.littleToNone)
-                        Text("Light").tag(Exercise.light)
-                        Text("Moderate").tag(Exercise.moderate)
-                        Text("Active").tag(Exercise.active)
-                        Text("Very Active").tag(Exercise.veryActive)
-                }
-                    .padding(10)
-                    .overlay(alignment: .leading){
-                        RoundedRectangle(cornerRadius: 16)
-                            .stroke(.gray, lineWidth: 1)
-                    
-                }
-               
-                }
-                .pickerStyle(.menu)
-                Picker("Gender", selection: $gender) {
-                    Text("Male").tag(Gender.male)
-                    Text("Female").tag(Gender.female)
-                }
-                .padding([.trailing, .leading], 10)
-                .padding([.top, .bottom], 10)
-                .pickerStyle(.segmented)
-                
-                NavigationLink(destination: CalorieCalculatorOutputView(userInfo: userInfo))  {
+                NavigationLink(destination: CalorieCalculatorOutputView(userInfo: userInfo)
+                    .navigationTitle(Text("Reccomended Calorie Intake"))
+                    .navigationBarTitleDisplayMode(.inline)){
                     Text("Calculate")
                         .foregroundColor(.white)
                         .padding()
@@ -92,21 +76,17 @@ struct CalorieCalculatorInput: View {
                     userInfo.exercise = exercise
                     
                 })
-                
-            }.navigationTitle(Text("Input User Info"))
+                Spacer()
 
+            }.navigationTitle(Text("Input User Info"))
+            
+           
+            }
+           
+      
         }
         
     }
-    func calculateUserInfo() -> Double {
-        let testHeight = Double(height) ?? 0
-        let testWeight = Double(weight) ?? 0
-        let testAge = Int(age) ?? 0
-        let userInfo: UserInfo = UserInfo(weight: testWeight, height: testHeight, gender: gender, age: testAge, exercise: exercise)
-        return userInfo.calculateCalorieIntake()
-    }
-
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
