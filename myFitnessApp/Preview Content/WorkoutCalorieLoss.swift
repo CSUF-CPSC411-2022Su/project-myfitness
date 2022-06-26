cc2306168aef6b2630c777646d966cee4c6bd6d7
Erica <ericab90@csu.fullerton.edu>
2022-06-26 13:22:48 -0700

    woah

--- myFitnessApp/Preview Content/WorkoutCalorieLoss.swift
+++ myFitnessApp/Preview Content/WorkoutCalorieLoss.swift
@@ -16,45 +16,8 @@ class GlobalModel: ObservableObject {
     @Published var time = ""
     @Published var weight = ""
 }
-struct CardioView: View {
-    @StateObject var global = GlobalModel.global
-    @State var isPresenting = false
-    //@StateObject weight = userInfo.CalorieCalculatorOutputView()
-    var body: some View {
-        VStack{
-            Text("Awesome job working out!")
-                .padding()
-            Text("Input the distance and time of cardio workout as well as weight to calculate calories lost")
-                .padding()
-            TextField("Enter the distance", text: $global.distance)
-            //let reps = Int($global.reps.text!) ?? 0
-            TextField("Enter time to complete exercise", text: $global.time)
-            TextField("Enter time to complete exercise", text: $global.weight)
-            Button("Calculate") {
-                isPresenting = true
-            }
-            NavigationLink(destination: CaloriesView(), isActive: $isPresenting) {EmptyView()}
-        }
-    }
-}
-struct StrengthView: View {
-    var body: some View {
-        //NavigationView {
-            List {
-                (Text("Choose strength workout"))
-                    .padding()
-                NavigationLink(destination: CoreView()) {
-                    Text("Core")
-                }
-                NavigationLink(destination: ArmsView()) {
-                    Text("Arms")
-                }
-                NavigationLink(destination: LegsView()) {
-                    Text("Legs")
-                }
-        }
-    }
-}
+
+
 struct CoreView: View {
     @StateObject var global = GlobalModel.global
     @State var isPresenting = false
