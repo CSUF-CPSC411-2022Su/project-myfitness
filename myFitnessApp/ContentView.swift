6b29014a8eaab47a3a8800670e516da36c09d169
Dacgameboy <dacortez101@csu.fullerton.edu>
2022-06-26 17:15:29 -0700

    pain

--- myFitnessApp/ContentView.swift
+++ myFitnessApp/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 import Foundation
 
 struct ContentView: View {
+    init(){
+        UITableViewCell.appearance().backgroundColor = .clear
+    }
+
     var body: some View {
         CalorieCalculatorInput()
+        
     }
 }
 
@@ -22,66 +27,85 @@ struct CalorieCalculatorInput: View {
     @AppStorage ("exercise") var exercise: Exercise = .littleToNone
     @AppStorage ("gender") var gender: Gender = .male
     var body: some View {
-        NavigationView {
-            VStack {
-                Form
-                {
-                    Section ("Weight") {
-                        TextField("Weight in TBD", text: $weight)
-                            .keyboardType(.decimalPad)
-                    }
-                    Section ("Height") {
-                        TextField("Height in TBD", text: $height)
-                            .keyboardType(.decimalPad)
-                    }
-                    Section ("Age") {
-                        TextField("Age in Years", text: $age)
-                            .keyboardType(.decimalPad)
-                    }
-                    Section ("Exercise") {
-                        Picker("Exercise", selection: $exercise) {
-                            Text("Little To None").tag(Exercise.littleToNone)
-                            Text("Light").tag(Exercise.light)
-                            Text("Moderate").tag(Exercise.moderate)
-                            Text("Active").tag(Exercise.active)
-                            Text("Very Active").tag(Exercise.veryActive)
-                        }
-                    }
-                    Section ("Gender") {
-                        Picker("Gender", selection: $gender) {
-                            Text("Male").tag(Gender.male)
-                            Text("Female").tag(Gender.female)
-                        }.pickerStyle(.segmented)
-                    }
-                }
-                NavigationLink(destination: CalorieCalculatorOutputView(userInfo: userInfo)
-                    .navigationTitle(Text("Reccomended Calorie Intake"))
-                    .navigationBarTitleDisplayMode(.inline)){
-                    Text("Calculate")
-                        .foregroundColor(.white)
-                        .padding([.leading, .trailing], 50)
-                        .padding([.top, .bottom], 10)
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                }.simultaneousGesture(TapGesture().onEnded{
-                    if let validWeight = Double(weight) {
-                        userInfo.weight = validWeight
-                    }
-                    if let validHeight = Double(height) {
-                        userInfo.height = validHeight
-                    }
-                    if let validAge = Int(age) {
-                        userInfo.age = validAge
-                    }
-                    userInfo.gender = gender
-                    userInfo.exercise = exercise
-                    
-                })
+        GeometryReader
+        {
+                    geometry in
+                    NavigationView
+                            {
+                                ZStack
+                                {
+                                    VStack {
+                                        Form
+                                        {
+                                            Section ("Weight") {
+                                                TextField("Weight in TBD", text: $weight)
+                                                    .keyboardType(.decimalPad)
+                                            }
+                                            Section ("Height") {
+                                                            TextField("Height in TBD", text: $height)
+                                                                .keyboardType(.decimalPad)
+                                                        }
+                                                        Section ("Age") {
+                                                            TextField("Age in Years", text: $age)
+                                                                .keyboardType(.decimalPad)
+                                                        }
+                                                        Section ("Exercise") {
+                                                            Picker("Exercise", selection: $exercise) {
+                                                                Text("Little To None").tag(Exercise.littleToNone)
+                                                                Text("Light").tag(Exercise.light)
+                                                                Text("Moderate").tag(Exercise.moderate)
+                                                        Text("Active").tag(Exercise.active)
+                                                        Text("Very Active").tag(Exercise.veryActive)
+                                                    }
+                                                }
+                                                Section ("Gender") {
+                                                    Picker("Gender", selection: $gender) {
+                                                        Text("Male").tag(Gender.male)
+                                                        Text("Female").tag(Gender.female)
+                                                    }.pickerStyle(.segmented)
+                                                }
+                                        }
+                                        .background(Color(.systemGroupedBackground))
+                                            NavigationLink(destination: CalorieCalculatorOutputView(userInfo: userInfo)
+                                                .navigationTitle(Text("Reccomended Calorie Intake"))
+                                                .navigationBarTitleDisplayMode(.inline)){
+                                                Text("Calculate")
+                                                    .foregroundColor(.white)
+                                                    .padding([.leading, .trailing], 50)
+                                                    .padding([.top, .bottom], 10)
+                                                    .background(Color.blue)
+                                                    .cornerRadius(10)
+                                            }.simultaneousGesture(TapGesture().onEnded{
+                                                if let validWeight = Double(weight) {
+                                                    userInfo.weight = validWeight
+                                                }
+                                                if let validHeight = Double(height) {
+                                                    userInfo.height = validHeight
+                                                }
+                                                if let validAge = Int(age) {
+                                                    userInfo.age = validAge
+                                                }
+                                                userInfo.gender = gender
+                                                userInfo.exercise = exercise
+                                                
+                                            })
 
-            }.navigationTitle(Text("User Info"))
-            
-           
-            }
+                                        }.navigationTitle(Text("User Info"))
+                                
+                               
+                            
+                        }
+                        .background( Image("green1")
+                            .scaledToFit()
+                            .edgesIgnoringSafeArea(.all)
+                            .frame(width: .infinity, height: .infinity)
+                            )
+                        
+                        
+                       
+                    }
+        }
+        
            
       
         }
