41db7386227622ec7c37aa1d56eddb64254be6e2
ericabCSUF <70290422+ericabCSUF@users.noreply.github.com>
2022-06-30 19:04:08 -0700

    Fixed the duplicate code

--- myFitnessApp/Workout/Cardio.swift
+++ myFitnessApp/Workout/Cardio.swift
@@ -9,6 +9,9 @@ import Foundation
 import SwiftUI
 
 struct CardioView: View {
+    //The WorkoutInfor() class contains all the necessary
+    //data to perform the calculations for the cardio 
+    //workout
     @ObservedObject var workoutInfo = WorkoutInfo()
     @AppStorage("time") var time = ""
     @AppStorage("dist") var distance = ""
@@ -16,7 +19,6 @@ struct CardioView: View {
     @State private var isCalculated = false
 
     var body: some View {
-        // GeometryReader { geometry in
         ZStack {
             VStack {
                 Image("cardiorun")
@@ -36,7 +38,6 @@ struct CardioView: View {
                         .modifier(TextFieldModifiers())
                         .keyboardType(.decimalPad)
                         .multilineTextAlignment(.center)
-                    // let reps = Int($global.reps.text!) ?? 0
                 } // End of Distance VStack
                 VStack {
                     Text("Enter time to complete exercise")
@@ -55,57 +56,56 @@ struct CardioView: View {
                         .modifier(TextFieldModifiers())
                         .keyboardType(.decimalPad)
                         .multilineTextAlignment(.center)
-                }
-                Button("Calculate") {
+                }// End of weight VStack
+                Button("Calculate"){
+                    //If statements to check input validation
                     if let validTime = Double(time) {
                         workoutInfo.time = validTime
                     }
-                    Button("Calculate"){
-                        if let validTime = Double(time) {
-                            workoutInfo.time = validTime
+                    if let validWeight = Double(weightkg) {
+                        workoutInfo.weightkg = validWeight
+                    }
+                    //A valid distance input will be checked in a switch
+                    //statement in the WorkoutCalorieLoss file and return
+                    //the appropriate MET value for calculation
+                    if let validDistance = Double(distance) {
+                        let check = workoutInfo.METcardiovalue
+                        if check > 0 {
+                            workoutInfo.met = check
+                            workoutInfo.distance = validDistance
+                            //Once the appropritate values are addigned, the function in 
+                            //WorkoutCalorieLoss file is called and will compute the total
+                            //calories lost after workout
+                            workoutInfo.weightLossCardio = workoutInfo.cardioWeightLoss()
                         }
-                        if let validWeight = Double(weightkg) {
-                            workoutInfo.weightkg = validWeight
-                        }
-                        if let validDistance = Double(distance) {
-                            let check = workoutInfo.METcardiovalue
-                            if check > 0 {
-                                workoutInfo.met = check
-                                workoutInfo.distance = validDistance
-                                workoutInfo.weightLossCardio = workoutInfo.cardioWeightLoss()
-                            }
-                        }
-                        isCalculated.toggle()
-                        hideKeyboard()
+                    }
+                    //Bool controls the text displaying the calories lost when Calculate is 
+                    // clicked
+                    isCalculated.toggle()
+                    hideKeyboard()
                     }.padding()
+                    //Text is hidden until the Calculate button is pressed and is displayed at 
+                    //bottom
                     Group {
                         if isCalculated {
                             Text("Calories lost: \(workoutInfo.weightLossCardio)")
-
                         }
                     }
-                    isCalculated.toggle()
-                    hideKeyboard()
-                }.padding()
-                Group {
-                    if isCalculated {
-                        Text("You lost \(workoutInfo.weightLossCardio)")
-                    }
-                }
-            } // End of VStack
-        } // End of ZStack
+            } //End of VStack
+        } //End of ZStack
+        //ZStack used to layer the VStacks on top of a background image
         .background(
             Image("green22222")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
                 .frame(width: 800, height: 850)
         )
-        // }//End of GeometryReader
+        //Pressing anywhere on the screen hides the keyboard
         .onTapGesture {
             hideKeyboard()
         }
-    } // End of body view
-} // End of View struct
+    } //End of body view
+} //End of View struct
 struct Cardio_Previews: PreviewProvider {
     static var previews: some View {
         CardioView()
